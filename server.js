637bb2377ef8671dd6fd6e2f22869687c79068ea
Peter Olsen <pto@me.com>
2011-03-17 19:28:50 -0400

    add OS info

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var http = require('http');
+var os = require('os');
 
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Hello, Node.js world!\n");
+  res.end("Running " + os.type() + " release " + os.release() +
+          " on " + os.hostname() + "\n");
 });
 
 server.listen(process.env.PORT || 8001);
