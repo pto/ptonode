b5e6249c4e8ccbaf25562d1336880ffc89913ae7
Peter Olsen <pto@me.com>
2011-03-20 13:51:34 -0400

    sorry, no cpus

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ var http = require("http");
 var os = require("os");
 
 var server = http.createServer(function (req, res) {
-  var cpus = os.cpus();
   res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Running " + os.type() + " release " + os.release() +
-          " on " + os.hostname() + " with " + cpus.length + " cpu" +
-          (cpus.length != 1 ? "s" : "") + " of type " +
-          cpus[0].model + " and speed " + cpus[0].speed + "\n");
+  res.end("Running node " + process.versions.node + " on " + os.type() +
+          " release " + os.release() +
+          " on " + os.hostname() + "\n");
 });
 
 server.listen(process.env.PORT || 8001);
