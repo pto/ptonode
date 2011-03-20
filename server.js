7b294719340ff6168de3caa57686d1bd646cda2e
Peter Olsen <pto@me.com>
2011-03-20 14:14:31 -0400

    add process

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var http = require("http");
 var os = require("os");
+var util = require("util");
 
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Running " + process.title + " " + process.version + 
+  res.write("Running " + process.title + " " + process.version + 
           " on " + os.type() +
           " release " + os.release() +
-          " at " + os.hostname() + "\n");
+          " at " + os.hostname() + "\n\n");
+  res.write("Process: " + util.inspect(process));
+  res.end();
 });
 
 server.listen(process.env.PORT || 8001);
