6460e140272c9a5d1ecc47ad6a8b4e9050036c12
Peter Olsen <pto@me.com>
2011-03-20 15:03:07 -0400

    add my first line of code

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.get('/', function(req, res){
 // Only listen on $ node app.js
 
 if (!module.parent) {
-  app.listen(3000);
+  app.listen(process.env.PORT || 8001);
   console.log("Express server listening on port %d", app.address().port);
 }
