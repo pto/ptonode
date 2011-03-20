686511e1cfdfef4fed16e4c478620d82a32a311e
Peter Olsen <pto@me.com>
2011-03-20 13:53:54 -0400

    getting node title

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var os = require("os");
 
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Running node " + process.versions.node + " on " + os.type() +
+  res.end("Running " + process.title + " " + process.version + 
+          " on " + os.type() +
           " release " + os.release() +
-          " on " + os.hostname() + "\n");
+          " at " + os.hostname() + "\n");
 });
 
 server.listen(process.env.PORT || 8001);
